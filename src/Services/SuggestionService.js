984baec1500675fcc2a12ed1a2fcf987e02341f0
AnhPT <tuananh200898@gmail.com>
2020-03-25 23:12:49 +0700

    up

--- src/Services/SuggestionService.js
+++ src/Services/SuggestionService.js
@@ -2,7 +2,7 @@ import axios from "../axios";
 
 export const searchCar = async () => {
   try {
-    const { data, status } = await axios.get("/posts");
+    const { data, status } = await axios.get("/suggestion");
     if (status !== 200) throw "Error";
 
     return data;
